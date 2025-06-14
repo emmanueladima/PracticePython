b3757ff2b4b71da1e8a2c3bc84ebfb1a0ca99796
Emmanuel Adima <emmanueladimac@gmail.com>
2025-06-14 11:02:50 -0700

    Solved extra requirements with reference to solution

--- PracticePython 2.py
+++ PracticePython 2.py
@@ -1,7 +1,17 @@
-number = int(input("Enter a number: "))
+number = int(input("Enter a number to check: "))
+check = int(input("Enter a number to divide by: "))
 
-if number % 2 != 0:
-    print(str(number) + " is not an even number.")
+if number % 4 == 0:
+    print(str(number) + " is a multiple of 4.")
+
+elif number % 2 == 0:
+    print(str(number) + " is an even number.")
 
 else:
-    print(str(number) + " is an even number")
+    print(str(number) + " is an odd number.")
+
+if number % check == 0:
+    print(number, "divides evenly by", check)
+
+else:
+    print(number, "does not divide evenly", check)
