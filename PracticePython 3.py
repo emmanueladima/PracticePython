7dc71853ca41ede78cd9f0678f8c944a88cf9f5c
Emmanuel Adima <emmanueladimac@gmail.com>
2025-06-14 12:36:58 -0700

    included faster method from solution

--- PracticePython 3.py
+++ PracticePython 3.py
@@ -1,17 +1,20 @@
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = []
-c = []
+#b = []
+#c = []
 
-for x in a:
-    if x < 5:
-        b.append(x)
+#for x in a:
+    #if x < 5:
+        #b.append(x)
 
-print(b)
+#print(b)
 
-value = int(input("Enter a value: "))
+#value = int(input("Enter a value: "))
 
-for i in a:
-    if i < value:
-        c.append(i)
+#for i in a:
+    #if i < value:
+       # c.append(i)
 
-print(c)
+#print(c)
+
+#this is a faster method 
+print( [x for x in a if x <5] )
